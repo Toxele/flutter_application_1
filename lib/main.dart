f322bfc31ac909d01c137328b5f2c5957ea2cb16
pack_dev <i.pack.dev@gmail.com>
2023-07-12 18:33:14 +0300

    upd: refactor code in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_final_fields, use_super_parameters, avoid_unnecessary_containers, sort_child_properties_last, avoid_print, avoid_dynamic_calls
-
-//import 'dart:js_util';
-//import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/strings.dart' as strings;
 import 'package:flutter_application_1/ui/graph_screen.dart';
 
-// ignore: always_use_package_imports
 import 'ui/home.dart';
 
-void main() {
-  runApp(GHFlutterApp());
-}
+void main() => runApp(const GHFlutterApp());
 
 class GHFlutterApp extends StatelessWidget {
-  const GHFlutterApp({Key? key}) : super(key: key);
+  const GHFlutterApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +15,7 @@ class GHFlutterApp extends StatelessWidget {
       title: strings.appTitle,
       initialRoute: '/',
       routes: {
-        '/': (context) => GHFlutter(),
+        '/': (context) => const GHFlutter(),
         '/graph': (context) => GraphScreen(),
       },
     );
