478ab143a7759589432f24839c9c24b401480d6a
pack_dev <i.pack.dev@gmail.com>
2023-08-08 18:59:19 +0300

    upd: format user_data_service

--- lib/domain/user_data_service.dart
+++ lib/domain/user_data_service.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_final_locals, file_names, cast_nullable_to_non_nullable, unused_field
 
 import 'dart:async';
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_application_1/domain/weather/weather.dart';
@@ -10,9 +11,13 @@ import 'model/user_record.dart';
 
 class UserDataService {
   UserDataService(this._serviceLoader);
+
   final JsonLoader _serviceLoader;
+
   List<UserRecord> _records = [];
-  List<UserRecord> get records => _records;
+  UnmodifiableListView<UserRecord> get records =>
+      UnmodifiableListView(_records);
+
   static const String _fileName = '\\records.json';
 
   Future<String> get _getPath async =>
@@ -36,12 +41,19 @@ class UserDataService {
     return [];
   }
 
-  Future addRecord(
-      {int sys = 120,
-      int dia = 80,
-      int pulse = 75,
-      required Weather weather}) async {
-    final user = UserRecord(sys: sys, dia: dia, pulse: pulse, weather: weather, timeOfRecord: DateTime.now());
+  Future addRecord({
+    int sys = 120,
+    int dia = 80,
+    int pulse = 75,
+    required Weather weather,
+  }) async {
+    final user = UserRecord(
+      sys: sys,
+      dia: dia,
+      pulse: pulse,
+      weather: weather,
+      timeOfRecord: DateTime.now(),
+    );
 
     _records.add(user);
     final recordsRaw = _records.map((e) => e.toJson()).toList();
