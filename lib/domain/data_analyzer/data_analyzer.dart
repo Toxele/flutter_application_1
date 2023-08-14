9a5f0a5dc6dcccd7fd6e857a7fbfed2f8bbefa8c
pack_dev <i.pack.dev@gmail.com>
2023-08-14 19:02:10 +0300

    add: DataAnalyzer

---
+++ lib/domain/data_analyzer/data_analyzer.dart
@@ -0,0 +1,24 @@
+import '../user_data_service.dart';
+
+/// todo должна быть freezed модель
+class AnalyzeModel {
+  String result = '';
+  String description = '';
+}
+
+/// todo: подумай над именованием всего
+class DataAnalyzer {
+  final UserDataService _userDataService;
+
+  const DataAnalyzer({
+    required UserDataService userDataService,
+  }) : _userDataService = userDataService;
+
+  AnalyzeModel analyzeData({required int countRecords}) {
+    _userDataService.records[countRecords];
+
+    /// todo глубокий анализ данных, расскиданных по разным методам
+
+    return AnalyzeModel();
+  }
+}
