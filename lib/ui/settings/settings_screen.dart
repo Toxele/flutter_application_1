7b84f38f9eeeb577cb5731e4008edce06303b446
pack_dev <i.pack.dev@gmail.com>
2023-09-14 18:12:15 +0300

    upd: remove storage provider from SettingsScreen

--- lib/ui/settings/settings_screen.dart
+++ lib/ui/settings/settings_screen.dart
@@ -1,34 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/data/storage_repository.dart';
-import 'package:provider/provider.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Provider<StorageRepository>(
-      builder: (context, child) => Scaffold(
-        appBar: AppBar(title: Text('Настройки')),
-        body: ListView(
-          children: [
-            ListTile(
-              title: Text('Важная настройка 1'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text('Важная настройка 1'),
-              onTap: () {},
-            ),
-            SwitchListTile(
-              value: true,
-              title: Text('bool настройка 1'),
-              onChanged: (value) {},
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(title: Text('Настройки')),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Важная настройка 1'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text('Важная настройка 1'),
+            onTap: () {},
+          ),
+          SwitchListTile(
+            value: true,
+            title: Text('bool настройка 1'),
+            onChanged: (value) {},
+          ),
+        ],
       ),
-      create: (_) => StorageRepository(),
     );
   }
 }
